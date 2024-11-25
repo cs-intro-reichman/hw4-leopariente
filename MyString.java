caf441513897e03eb5c9eaaadacb56ac60bce044
Leo Pariente <leopariente@gmail.com>
2024-11-25 17:14:02 +0200

    Fix

--- MyString.java
+++ MyString.java
@@ -18,13 +18,50 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lowerCaseStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            int numToAdd = 0;
+            if (currentChar > 64 && currentChar < 91) {
+                numToAdd = 32;
+            }
+            lowerCaseStr += (char) (currentChar + numToAdd);
+        }
+        return lowerCaseStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+                if ((int) str1.charAt(i + j) != (int) str2.charAt(j)) {
+                    break;
+                }
+                if (j == str2.length() - 1) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
+
+    // /** If str1 contains str2, returns true; otherwise returns false. */
+    // public static boolean contains(String str1, String str2) {
+    // int counter = 0;
+    // for (int i = 0; i < str1.length(); i++) {
+    // if (counter + 1 == str2.length()) {
+    // return true;
+    // }
+    // if (i == str2.length()) {
+    // return str2.length() == counter + 1;
+    // }
+    // if ((int) str1.charAt(i) == (int) str2.charAt(counter)) {
+    // counter++;
+    // } else {
+    // counter = 1;
+    // }
+    // }
+    // return str2.length() == counter + 1;
+    // }
 }
