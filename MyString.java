4cba8a2af75f1eca70a4d8464ae97357d249b801
Leo Pariente <leopariente@gmail.com>
2024-11-27 11:56:40 +0200

    Fix

--- MyString.java
+++ MyString.java
@@ -32,7 +32,8 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-
+        if (str2.length() == 0)
+            return true;
         for (int i = 0; i < str1.length(); i++) {
             for (int j = 0; j < str2.length(); j++) {
                 if ((int) str1.charAt(i + j) != (int) str2.charAt(j)) {
